d64d94c9329e9c54df79dbc52b56d87540aa7817
ryanbabida <ryan.babida@gmail.com>
2015-11-12 21:36:56 -0800

    added scales, need to fix user inputs

--- scales.py
+++ scales.py
@@ -1,5 +1,7 @@
 def getScale(notes, frequencies, scales_choices):
   root = notes[scales_choices[0]]
+  
+  scale = list()
 
   if scales_choices[1] == 1:
     scale = [frequencies[root], frequencies[root + 2], 
@@ -7,7 +9,7 @@ def getScale(notes, frequencies, scales_choices):
            frequencies[root + 7], frequencies[root + 9], 
            frequencies[root + 11], frequencies[root + 12]]
 
-  else:
+  elif scales_choices[1] == 2:
     scale = [frequencies[root], frequencies[root + 2], 
            frequencies[root + 3], frequencies[root + 5],
            frequencies[root + 7], frequencies[root + 8], 
